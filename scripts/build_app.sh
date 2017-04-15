a211cfed9abf4d1fff0955777c98e623b8c2979c
Alex Tsankov <antsankov@gmail.com>
2017-04-15 13:44:00 -0600

    Chain var path

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+chain_dir=$HOME/.monax/chains/firstchain
+chain_dir_this=$chain_dir/firstchain_full_000
 addr=$(cat $chain_dir/addresses.csv | grep firstchain_full_000 | cut -d ',' -f 1) && echo $addr
 sleep 2
 monax pkgs do --chain firstchain --address $addr
 
-cp $HOME/.monax/chains/firstchain/accounts.json .
+cp $chain_dir/accounts.json .
 npm install
